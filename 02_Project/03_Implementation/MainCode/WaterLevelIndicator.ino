ed0a5132eb42310a8169f366166630771b4e49f1
Sarang Nasare <sarang.nasare.com>
2022-03-04 16:01:37 +0530

    commit

--- 02_Project/03_Implementation/MainCode/WaterLevelIndicator.ino
+++ 02_Project/03_Implementation/MainCode/WaterLevelIndicator.ino
@@ -1,42 +1,92 @@
-#include <NewPing.h>
 #include <Servo.h>
-#define trigPin 12
-#define ecoPin 13
-#define maxdistance 50
-NewPing sonar (trigPin,ecoPin,maxdistance);
-int LEDijau=3,LEDmerah=2;
+#define trigpin 7
+#define echopin 6
+int led1 = A0;
+int led2 = A1;
+int led3 = A2;
+int led4 = A3;
+int led5 = A4;
 Servo myservo;
-
-void setup(){
-  Serial.begin(115200);
-  pinMode(trigPin, OUTPUT);
-  pinMode(ecoPin, INPUT);
-  pinMode(LEDmerah, OUTPUT);
-  pinMode(LEDijau, OUTPUT);
-  myservo.attach(9);
+void setup()
+{
+Serial.begin(9600);
+pinMode(trigpin, OUTPUT);
+pinMode(echopin, INPUT);
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT);
+pinMode(led4, OUTPUT);
+pinMode(led5, OUTPUT);
+myservo.attach(9);
+digitalWrite(led1, LOW);
+digitalWrite(led2, LOW);
+digitalWrite(led3, LOW);
+digitalWrite(led4, LOW);
+digitalWrite(led5, LOW);
+delay(1000);
 }
-void loop(){
-int duration,jarak,posisi=0,i;
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-duration=pulseIn(ecoPin, HIGH);
-jarak=(duration/2);
- Serial.print(jarak);
- Serial.println(" cm");
-
-if(jarak<=5)
-  {
-    digitalWrite(LEDmerah, LOW);
-    digitalWrite(LEDijau, HIGH);
-    myservo.write(180);
-  }
-else{
-  digitalWrite(LEDijau, LOW);
-  digitalWrite(LEDmerah, HIGH);
-  myservo.write(90);
-  }
-delay(450);  
+void loop()
+{
+int duration, distance;
+digitalWrite(trigpin, HIGH);
+delayMicroseconds(1000);
+digitalWrite(trigpin, LOW);
+duration = pulseIn(echopin,HIGH);
+distance = ( duration / 2) / 29.1;
+Serial.println("cm:");
+Serial.println(distance);
+if( (distance > 0) && (distance <= 10) )
+{
+digitalWrite(led1, HIGH);
+digitalWrite(led2, HIGH);
+digitalWrite(led3, HIGH);
+digitalWrite(led4, HIGH);
+digitalWrite(led5, HIGH);
+myservo.write(90);
+} else
+if( (distance > 10) && (distance <= 20) )
+{
+digitalWrite(led1, LOW);
+digitalWrite(led2, HIGH);
+digitalWrite(led3, HIGH);
+digitalWrite(led4, HIGH);
+digitalWrite(led5, HIGH);
+myservo.write(90);
+} else
+if( (distance > 20) && (distance <= 30) )
+{
+digitalWrite(led1, LOW);
+digitalWrite(led2, LOW);
+digitalWrite(led3, HIGH);
+digitalWrite(led4, HIGH);
+digitalWrite(led5, HIGH);
+myservo.write(90);
+} else
+if( (distance > 30) && (distance <= 40) )
+{
+digitalWrite(led1, LOW);
+digitalWrite(led2, LOW);
+digitalWrite(led3, LOW);
+digitalWrite(led4, HIGH);
+digitalWrite(led5, HIGH);
+myservo.write(90);
+} else
+if( (distance > 50) && (distance <= 60) )
+{
+digitalWrite(led1, LOW);
+digitalWrite(led2, LOW);
+digitalWrite(led3, LOW);
+digitalWrite(led4, LOW);
+digitalWrite(led5, HIGH);
+myservo.write(90);
+} else
+if( distance > 60 )
+{
+digitalWrite(led1, LOW);
+digitalWrite(led2, LOW);
+digitalWrite(led3, LOW);
+digitalWrite(led4, LOW);
+digitalWrite(led5, LOW);
+myservo.write(180);
+}
 }
